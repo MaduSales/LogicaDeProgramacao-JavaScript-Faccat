c952733586e669dfdde900d3695a1dcc68c1517a
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-21 20:34:36 -0300

    Add files via upload

---
+++ Laco while/script.js
@@ -0,0 +1,10 @@
+// Enquanto o valor do contador (inicialmente 0) for menor do que 8, o nome digitado se repetirá
+let contadora = 0;
+
+
+while (contadora < 8){
+    console.log("Jailson" + contadora);
+    contadora++
+}
+
+
