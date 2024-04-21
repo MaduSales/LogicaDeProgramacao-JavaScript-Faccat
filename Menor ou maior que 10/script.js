baa3966ae146eb5c092824f9c4aeb73af3c91523
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-04-21 20:54:18 -0300

    Add files via upload

---
+++ Menor ou maior que 10/script.js
@@ -0,0 +1,13 @@
+// Informa se o número digitado é maior ou menor que 10
+
+let numero = parseInt(prompt("Digite um número"));
+
+if (numero > 10){
+    alert("É MAIOR QUE 10!");
+} else if (numero == 10){
+    alert("É IGUAL A 10!");
+} else{
+    alert("É MENOR QUE 10!");
+}
+
+
